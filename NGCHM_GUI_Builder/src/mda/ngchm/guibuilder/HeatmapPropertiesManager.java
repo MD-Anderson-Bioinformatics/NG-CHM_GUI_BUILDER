515ba4e9100a749c55cc25db7b9c539b89cdfd0f
Bradley Broom <bmbroom@mdanderson.org>
2021-12-09 14:06:43 -0600

    Update GUI Builder version number
    Release fixes incorrect extraction under some circumstances of covariates
    included in the main data file.

--- NGCHM_GUI_Builder/src/mda/ngchm/guibuilder/HeatmapPropertiesManager.java
+++ NGCHM_GUI_Builder/src/mda/ngchm/guibuilder/HeatmapPropertiesManager.java
@@ -25,7 +25,7 @@ public class HeatmapPropertiesManager {
 		public Order col_configuration;
 		public Order row_configuration;
 		public String output_location;
-		public String builder_version = "NG-CHM GUI 2.20.2";
+		public String builder_version = "GUI Builder 2.20.3 w/ NG-CHM 2.20.2";
 		public String summary_width = "40";
 		public BuilderConfig builder_config;
 	}
