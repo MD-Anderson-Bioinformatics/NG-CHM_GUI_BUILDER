36068920a047794c9f72a841401a9465c44ee34f
Bradley Broom <bmbroom@mdanderson.org>
2023-03-21 14:58:40 -0500

    Fetch NG-CHM artifacts from a Git repository at build time
    - Remove NG-CHM artifacts from this git repository.
    - Put GUI Builder version into a file by itself.
    - Use tag for fetching NG-CHM artifacts as the NG-CHM version number.
    - Set builder version string and update ?v= parameters during build.
      - Ensures the ?v= values are always up to date.
      - Sets the ?v= value to the md5 checksum of the referenced file.
      - Works automatically during development.
      - Only invalidates cached files for files that have changed.
      - No need to update all HTML files in the git repo on version bumps.

--- build/NGCHM_GUI_Builder/src/mda/ngchm/guibuilder/BuilderVersion.java
+++
@@ -1,5 +0,0 @@
-package mda.ngchm.guibuilder;
-
-public class BuilderVersion {
-    public static final String version = "GUI Builder 2.21.1 w/ NG-CHM 2.23.3";
-}
---
+++ build/versioner-v2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+GUIBUILDER=/NGCHM_GUI_Builder
+VIEWER_TAG=$1
+
+# Update GUI Builder version string.
+BUILDER_VERSION=$(cat $GUIBUILDER/VersionNumber)
+SRCDIR=$GUIBUILDER/src/mda/ngchm/guibuilder
+sed -e "s|VERSIONSTRING|GUI Builder ${BUILDER_VERSION} w/ NG-CHM ${VIEWER_TAG}|" < $SRCDIR/BuilderVersion.template > $SRCDIR/BuilderVersion.java
+
+# Update ?v= values to files referenced in HTML files.
+cd $GUIBUILDER/WebContent
+FILES_TO_UPDATE=$(grep '?v=' *html | sed 's/:.*//'| uniq)
+
+# Declare md5sums as an associative array so that we don't have
+# to recompute the md5sums of files referenced in multiple
+# HTML files.
+declare -A md5sums=()
+
+# Create a temporary file for containing file references during
+# each HTML file check.
+tmpfile=$(mktemp /tmp/filerefs.XXXXXX)
+
+for file in $FILES_TO_UPDATE ; do
+    echo Checking ${file}:
+    # Find references in $file that have to be checked.
+    grep '="[a-zA-Z][^"]*?v=.*"' $file > $tmpfile
+    while read -r ref ; do
+	# For each reference
+	# Get the referenced file and its current ?v= value.
+	subfile=$(echo $ref | sed -e 's/.*="//' -e 's/?v=.*//')
+	currentv=$(echo $ref | sed -e 's/.*?v=//' -e 's/".*//')
+	# Determine its md5sum if we haven't already
+	if [ -z "${md5sums[$subfile]}" ] ; then
+	    md5sums+=( [$subfile]=$(md5sum < "$subfile" | sed 's/ *-//') )
+	fi
+	# Update the ?v= value if the md5sum has changed
+	if [ x$currentv != x${md5sums[$subfile]} ] ; then
+	    sed -i 's:"'"$subfile?v="'[^"]*":"'"$subfile?v=${md5sums[$subfile]}"'":' $file
+	fi
+    done < $tmpfile
+done
+
+# Clean up the temporary file.
+rm -f ${tmpfile}
+
+exit 0
